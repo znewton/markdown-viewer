e7bf4a0ad4f914bae4a757b2d7b378d9a3a85c73
Zach Newton <znewton@iastate.edu>
2017-11-01 14:30:50 -0500

    main process

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ const url = require('url')
 let win;
 
 function createWindow () {
-  win - new BrowserWindow({width: 800, height: 600});
-  
+  win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }))
+  win.on('closed', () => {win = null})
 }
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  if (win == null) {
+    createWindow()
+  }
+})
+
+document.write('testing');
